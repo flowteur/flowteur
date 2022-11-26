ff580279daab07caa6b4079dd43c84a3447430cc
Yassin Siouda <yassin@siouda.com>
2022-11-26 22:23:17 +0100

    import pipelines only on function call

--- back/modules/gpt.py
+++ back/modules/gpt.py
@@ -3,24 +3,11 @@ from transformers import GPT2LMHeadModel, GPT2Tokenizer
 # get env var for token
 huggingface_token = os.environ.get("HUGGINGFACE_TOKEN")
 
-notLoaded = True
-
-def load():
-    global notLoaded
-    global tokenizer
-    global model
-    if notLoaded:
-        tokenizer = GPT2Tokenizer.from_pretrained("sberbank-ai/mGPT", use_auth_token=huggingface_token)
-        model = GPT2LMHeadModel.from_pretrained("sberbank-ai/mGPT", use_auth_token=huggingface_token)
-
-        notLoaded = False
-        return tokenizer, model   
-    return tokenizer, model
-
 
 def generate(text, min_length=100, max_length=100, eos_token_id=5, pad_token=1, top_k=10, top_p=0.0, no_repeat_ngram_size=5):
-    # get the tokenizer and model and out
-    tokenizer, model = load()
+    tokenizer = GPT2Tokenizer.from_pretrained("sberbank-ai/mGPT", use_auth_token=huggingface_token)
+    model = GPT2LMHeadModel.from_pretrained("sberbank-ai/mGPT", use_auth_token=huggingface_token)
+
     
     input_ids = tokenizer.encode(text, return_tensors="pt")
     out = model.generate(
--- back/modules/stable_diffusion.py
+++ back/modules/stable_diffusion.py
@@ -5,17 +5,18 @@ from PIL import Image
 # get env var for token
 huggingface_token = os.environ.get("HUGGINGFACE_TOKEN")
 
-
-notLoaded = True
-
 model_id = "CompVis/stable-diffusion-v1-4"
-pipe = StableDiffusionPipeline.from_pretrained(model_id, low_cpu_mem_usage=True, use_auth_token=huggingface_token)
-pipe = pipe.to("cpu")
-def dummy(images, **kwargs):
-    return images, False
-pipe.safety_checker = dummy
+
 
 
 def generate(prompt, num_inference_steps=16, width=256, height=256):
+    pipe = StableDiffusionPipeline.from_pretrained(model_id, low_cpu_mem_usage=True, use_auth_token=huggingface_token)
+    pipe = pipe.to("cpu")
+    
+    # remove nsfw filter
+    def dummy(images, **kwargs):
+        return images, False
+    
+    pipe.safety_checker = dummy
     image = pipe(str(prompt), num_inference_steps=int(num_inference_steps), width=int(width), height=int(height)).images[0]
     return image
